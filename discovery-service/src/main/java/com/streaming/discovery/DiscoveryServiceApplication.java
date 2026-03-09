b795ea2f9fc1ec1de678cb3a7233156aad9f2975
MohamedMoustir <itsmoustir@gmail.com>
2026-03-09 21:45:52 +0000

    feat(discovery): initialize eureka discovery server

---
+++ discovery-service/src/main/java/com/streaming/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,18 @@
+package com.streaming.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Discovery Service - Point central d'enregistrement des microservices.
+ * Accessible sur http://localhost:8761
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
+
